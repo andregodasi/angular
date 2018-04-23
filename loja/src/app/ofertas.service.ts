038c88a8ac9c2dd81605a78b12e12ba77d5f0570
andregodasi <andre.godasi@gmail.com>
2018-04-22 22:14:37 -0300

    Inicio do segundo projeto => loja

---
+++ loja/src/app/ofertas.service.ts
@@ -0,0 +1,21 @@
+import { Http } from '@angular/http';
+import { Oferta } from './shared/oferta.model';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class OfertasService {
+
+  constructor(private http: Http) {
+
+  }
+
+
+  public getOfertas(): Promise<Array<Oferta>> {
+    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+      .toPromise()
+      .then((ofertas: any) => ofertas.json())
+      .catch(e => console.log(e));
+  }
+
+}
